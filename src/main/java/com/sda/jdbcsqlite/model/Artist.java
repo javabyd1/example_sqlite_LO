d5e5d036f6ce1a35615799fc55d0023e824205c2
lukaszogn <lukaszogan@mat.umk.pl>
2017-11-04 15:24:41 +0100

    create repo

--- src/main/java/com/sda/jdbcsqlite/model/Artist.java
+++ src/main/java/com/sda/jdbcsqlite/model/Artist.java
@@ -20,4 +20,12 @@ public class Artist {
     public void setName(String name) {
         this.name = name;
     }
+
+    @Override
+    public String toString() {
+        return "Artist{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
